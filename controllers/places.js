8d6ff81ec9a23951cdec89b5e0d3359310190c12
Agusienka <101678267+Agusienka@users.noreply.github.com>
2022-07-30 09:29:28 -0500

    adding places

--- controllers/places.js
+++ controllers/places.js
@@ -1,4 +1,5 @@
 const router = require('express').Router()
+const places = require('../model/places.js')
 
 router.get('/', (req, res) => {
         let places = [{
@@ -22,6 +23,34 @@ router.get('/new', (req, res) => {
   res.render('places/new')
 })
 
+router.get('/:id', (req, res) => {
+  db.Place.findOne({ _id: req.params.id })
+      .populate('comments')
+      .then(place => {
+          console.log(place.comments)
+          res.render('places/show', { place })
+      })
+      .catch(err => {
+          console.log('err', err)
+          res.render('error404')
+      })
+})
+
+router.post('/', (req, res) => {
+  console.log(req.body)
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
+  places.push(req.body)
+  res.redirect('POST /places')
+})
 
 
 
--- index.js
+++ index.js
@@ -9,6 +9,9 @@ app.use(express.static('public'))
 
 app.use('/places', require('./controllers/places.js'))
 
+app.use(express.urlencoded({ extended: true }))
+
+
 app.get('/', (req, res) => {
     res.render('home')
 

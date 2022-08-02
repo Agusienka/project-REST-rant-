74e02cb57f024687b0cd26579e4ca52730f3a7f4
Agusienka <101678267+Agusienka@users.noreply.github.com>
2022-08-02 09:21:05 -0500

    choose the state

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -32,8 +32,76 @@ function new_form (data) {
                   <input className="form-control" id="city" name="city" />
                 </div>
                 <div className="form-group col-sm-4">
-                  <label htmlFor="state">State</label>
-                  <input className="form-control" id="state" name="state" />
+                <label htmlFor="state"></label>
+                <link rel="stylesheet" href="css/style.css" />
+
+  <div class="container form-group">
+                
+                
+                
+  <div id="wrapper">
+    <div class="demo">
+     
+      <div class="form-group">
+      
+        <select id="select-state" className="form-select col-sm-4 "  >
+          <option value="">Select a state</option>
+          <option value="AL">Alabama</option>
+          <option value="AK">Alaska</option>
+          <option value="AZ">Arizona</option>
+          <option value="AR">Arkansas</option>
+          <option value="CA">California</option>
+          <option value="CO">Colorado</option>
+          <option value="CT">Connecticut</option>
+          <option value="DE">Delaware</option>
+          <option value="DC">District of Columbia</option>
+          <option value="FL">Florida</option>
+          <option value="GA">Georgia</option>
+          <option value="HI">Hawaii</option>
+          <option value="ID">Idaho</option>
+          <option value="IL">Illinois</option>
+          <option value="IN">Indiana</option>
+          <option value="IA">Iowa</option>
+          <option value="KS">Kansas</option>
+          <option value="KY">Kentucky</option>
+          <option value="LA">Louisiana</option>
+          <option value="ME">Maine</option>
+          <option value="MD">Maryland</option>
+          <option value="MA">Massachusetts</option>
+          <option value="MI">Michigan</option>
+          <option value="MN">Minnesota</option>
+          <option value="MS">Mississippi</option>
+          <option value="MO">Missouri</option>
+          <option value="MT">Montana</option>
+          <option value="NE">Nebraska</option>
+          <option value="NV">Nevada</option>
+          <option value="NH">New Hampshire</option>
+          <option value="NJ">New Jersey</option>
+          <option value="NM">New Mexico</option>
+          <option value="NY">New York</option>
+          <option value="NC">North Carolina</option>
+          <option value="ND">North Dakota</option>
+          <option value="OH">Ohio</option>
+          <option value="OK">Oklahoma</option>
+          <option value="OR">Oregon</option>
+          <option value="PA">Pennsylvania</option>
+          <option value="RI">Rhode Island</option>
+          <option value="SC">South Carolina</option>
+          <option value="SD">South Dakota</option>
+          <option value="TN">Tennessee</option>
+          <option value="TX">Texas</option>
+          <option value="UT">Utah</option>
+          <option value="VT">Vermont</option>
+          <option value="VA">Virginia</option>
+          <option value="WA">Washington</option>
+          <option value="WV">West Virginia</option>
+          <option value="WI">Wisconsin</option>
+          <option value="WY">Wyoming</option>
+        </select>
+      </div>
+    </div>
+  </div>
+</div>
                 </div>
                 <div className="form-group col-sm-4">
                   <label htmlFor="founded">Founded Year</label>
@@ -47,6 +115,7 @@ function new_form (data) {
               <input className="btn btn-primary" type="submit" value="Add Place" />
             </form>
           </main>
+          
         </Def>
     )
 }

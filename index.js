529c987b3f81cdc6bf478ea0c2d533d82ffba72c
Agusienka <101678267+Agusienka@users.noreply.github.com>
2022-08-10 20:23:02 -0500

    working for now, lol

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config()
 const express = require('express')
 const methodOverride = require('method-override')
 const app = express()
@@ -6,40 +6,24 @@ const app = express()
 
 //express settings
 // Express Settings
-app.set('views', __dirname + '/views')
+
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
-
-
+// Controllers & Routes
+app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
     res.render('home')
-
-    
 })
 
 app.get('*', (req, res) => {
-    res.render ('error404')
+    res.render('error404')
 })
 
+// Listen for Connections
 app.listen(process.env.PORT)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
